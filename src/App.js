965573ca295c7c11712f3c0be2e97b18042e56c4
Jonathan Picques <jonathan.picques@gmail.com>
2019-02-26 18:17:17 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import { User } from "./User";
+import { Login } from "./Login";
 
 class App extends Component {
+  state = {
+    user: undefined
+  };
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    if (this.state.user === undefined) {
+      return <Login onLoginSuccess={this.handleUserChange} />;
+    }
+    return <User user={this.state.user} />;
   }
+
+  handleUserChange = user => {
+    this.setState({ user: user });
+  };
 }
 
-export default App;
+export { App };
